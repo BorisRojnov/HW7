07abb70a845b7725d8020d4810416696684ff4b9
BorisRojnov <BorisRojnov@yandex.by>
2024-08-10 10:27:27 +0300

    homework complete(another version)

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
-        for(int i =2; i <= 100; i = i +2){
+        int i = 2;
+        while(i <= 100){
             System.out.println("Уволен айтишник с id " + i);
+            i = i + 2;
         }
     }
 }
